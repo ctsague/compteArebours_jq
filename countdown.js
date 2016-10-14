4dc1dc779b4071bd0a2315be5a83d9dc84a8436e
tsague <tsagueymele@yahoo.fr>
2016-10-14 12:32:51 +0200

    ajout minutes et secondes

--- countdown.js
+++ countdown.js
@@ -1,13 +1,23 @@
-
 function compteur(){
-	count--;
-	//console.log(count);
-	 $('.count').text(count);		
+	timer--;
+	
+	var minutes = parseInt(timer/60, 10);
+	var secondes = (timer - minutes*60);
+
+	 $('#minutes').text(minutes);	
+	 $('#secondes').text(secondes);	
+	 console.log(minutes);
+	 console.log(secondes);
+
 }
 
-var count=100;
+var timer=100;
 var compte =setInterval(compteur, 1000);
+
+
+
 
 $('.reset').on('click',function(){
 	clearInterval(compte);
 });
+
